7c037777b5e4cadf5967ffb4df7b09870ec0c06a
root <root@debian-s-1vcpu-1gb-london1-01.localdomain>
2018-06-16 22:10:24 +0000

    frist

---
+++ basic.js
@@ -0,0 +1,17 @@
+variable = "varIabLe";
+console.log(`This is one way to pass a ${variable}`);
+console.log('This is another way to pass a ' + variable);
+console.log('Let\'s check types:\n');
+var number = 5.1234098098098345098153451;
+console.log(`${number} is a type of ` + typeof number);
+var string = "Stringy" + "cheese";
+var num2str = 1 + "cat";
+var array = [];
+console.log(`${string} is a ` + typeof string);
+console.log(`${num2str} is a ` + typeof num2str + ` where we concatenate a number and a string`);
+console.log(`${array} is an empty ` + typeof array);
+var rando = Math.random();
+console.log('Generate a random number: ' + rando);
+var rando1 = rando * 10;
+console.log('Make it between 1 and 10:' + rando1);
+console.log(Object.getOwnPropertyNames(Math));
